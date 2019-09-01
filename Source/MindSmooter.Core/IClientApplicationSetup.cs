232387cff958c6eea9757ea126e49f491e588e46
Stian <Stian_borseth@hotmail.com>
2019-09-01 13:53:59 +0200

    Initial Blank Project
    Setup an initial Blank Visual Studio Solution with Desktop, mobile (Android+iOS) and Core folders. Implemented and tested working empty projects.

---
+++ Source/MindSmooter.Core/IClientApplicationSetup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/// <summary>
+/// The core functionality shared between all parts of the MindSmoother application
+/// </summary>
+namespace MindSmoother.Core
+{
+    /// <summary>
+    /// The entry point into the MindSmoother application used to setup the environment.
+    /// All top-level applications should create an instance of this
+    /// </summary>
+    public interface IClientApplicationSetup
+    {
+    }
+}
